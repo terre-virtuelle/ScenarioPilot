e0c918cd5d4068edc4f56a8864dbc75182ba3f2e
niktopelo <niktopelo@gmail.com>
2022-03-15 12:38:55 +0100

    change main port

--- quasar.conf.js
+++ quasar.conf.js
@@ -75,7 +75,7 @@ module.exports = configure(function (ctx) {
       server: {
         type: "http",
       },
-      port: 8080,
+      port: 8585,
       open: true, // opens browser window automatically
     },
 
